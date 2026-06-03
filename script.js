e4e698403e6466f097454c90d8dca10e92a781c0
sainidhruv2026 <sainidhruv.2026@gmail.com>
2026-06-03 16:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+const header = document.querySelector(".site-header");
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) header.classList.add("scrolled");
+  else header.classList.remove("scrolled");
+});
+
+menuBtn?.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => navLinks.classList.remove("open"));
+});
+
+const animate = () => {
+  const items = document.querySelectorAll(".reveal");
+  const trigger = window.innerHeight * 0.88;
+
+  items.forEach(item => {
+    const top = item.getBoundingClientRect().top;
+    if (top < trigger) {
+      item.style.opacity = 1;
+      item.style.transform = "translateY(0)";
+      item.style.transition = "all .8s ease";
+    }
+  });
+};
+
+window.addEventListener("scroll", animate);
+window.addEventListener("load", animate);
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+
+}, 5000);
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("door-loader");
+
+    // 0.5 sec baad door open hona start
+    setTimeout(() => {
+        loader.classList.add("open");
+    }, 500);
+
+    // Total 3 sec me loader remove
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 3000);
+
+});
+window.addEventListener("scroll", function(){
+
+    const header = document.querySelector(".site-header");
+
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+
+});
+const doorCursor = document.getElementById("doorCursor");
+
+document.addEventListener("mousemove", (e)=>{
+    doorCursor.style.left = e.clientX + "px";
+    doorCursor.style.top = e.clientY + "px";
+});
